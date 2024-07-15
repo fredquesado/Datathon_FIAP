680681f5cd5e42a528fedcb746bfd3e4a83deee7
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-07-14 23:57:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import streamlit as st
+from pages import 1_Grupo, 2_Desafio, 3_Base_Histórica, 4_Previsão, 5_Insights_e_Conclusão
+
+# Mapeamento das páginas
+PAGES = {
+    "Home": Home,
+    "Grupo": 1_Grupo,
+    "Desafio": 2_Desafio,
+    "Base Histórica": 3_Base_Histórica,
+    "Previsão": 4_Previsão,
+    "Insights e Conclusão": 5_Insights_e_Conclusão,
+}
 
 def main():
-    st.title("Meu Aplicativo Streamlit")
-    st.write("Bem-vindo ao meu aplicativo Streamlit!")
+    st.sidebar.title('Menu de Navegação')
+    selection = st.sidebar.radio("Ir para", list(PAGES.keys()))
+    
+    page = PAGES[selection]
+    page.app()
 
 if __name__ == "__main__":
     main()
