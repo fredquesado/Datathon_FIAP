803aa537d15326a7cd25bdb27d666ffc90cdb99c
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-07-14 22:04:47 -0300

    Update and rename 5_Conclusão.py to 5_Insights e Conclusão.py

---
+++ pages/5_Insights e Conclusão.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title('Insights e Conclusão')
+
+st.markdown("""
+# Insights
+""", unsafe_allow_html=True)
+
+st.markdown("""
+## O projeto proporcionou diversos insights valiosos:
+
+**• Influência Geopolítica nos Preços do Petróleo:** O preço do barril de petróleo é fortemente influenciado pela geopolítica global. Isso é evidente nos anos de 1990-1991, durante a Guerra do Golfo, e novamente em 2008, com a crise financeira global. Esses eventos causaram flutuações significativas nos preços devido à incerteza e às mudanças na oferta e demanda.
+
+**• Relação com a Força do Dólar:** Existe uma relação inversa entre o preço do petróleo e a força do dólar. Na análise inicial, observamos que quando o dólar se fortalece, os preços do petróleo tendem a cair.
+
+**• Validade e Limitações dos Modelos Preditivos:** Modelos de previsão de preços do petróleo, como o Random Forest Regressor utilizado neste projeto, são valiosos para embasar decisões estratégicas de curto e médio prazo. No entanto, a análise mostrou que a precisão do modelo diminui para previsões mais longas.
+
+**• Impacto das Políticas de Energia Renovável:** As políticas globais de transição para energias renováveis também têm um impacto significativo nos preços do petróleo.
+""", unsafe_allow_html=True)
