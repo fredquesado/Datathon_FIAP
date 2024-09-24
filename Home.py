0438ad8ddc484aa375cc7c431d2c6c221111ccaf
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-09-24 17:40:19 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 def app():
-    st.sidebar.success("Selecione uma página acima.")
-    st.title('Bem-vindo(a)')
-    st.markdown('<h1 style="font-family:Arial; font-size:60px; color:Black;">Datathon - Fiap</h1>', unsafe_allow_html=True)
-    st.markdown('<h1 style="font-family:Arial; font-size:20px; color:Black;">Transformando dados em impacto: previsão e análise para uma educação mais inclusiva e eficaz</h1>', unsafe_allow_html=True)
+    st.title("Página Home")
+    st.write("Bem-vindo à página inicial.")
