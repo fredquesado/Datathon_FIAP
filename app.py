b78c69b0670172d00fbe553fb2fff5d46d7984c2
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-09-24 00:50:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import 1_Grupo, 2_Desafio, 3_Relatorio_Analitico_preditivo, 4_Insights_e_Conclusão
+from pages import Home, 1_Grupo, 2_Desafio, 3_Relatorio_Analitico_preditivo, 4_Insights_e_Conclusão
 
 # Mapeamento das páginas
 PAGES = {
