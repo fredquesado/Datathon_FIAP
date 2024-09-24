8c169905a80227f6dbaf4e5dab4fe196fe3b411a
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-09-24 15:40:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,5 @@ PAGES = {
     "Insights": insights_conclusao,
 }
 
-def main():
-    st.sidebar.title('Menu de Navegação')
-    selection = st.sidebar.radio("Ir para", list(PAGES.keys()))  # Exibe os títulos corretos no menu
-    page = PAGES[selection]
-    page.app()  # Chama a função app() de cada página
-
 if __name__ == "__main__":
     main()
