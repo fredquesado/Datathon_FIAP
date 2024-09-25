4c6313f1fb24f48f3161db9636aba5d7011c9fbe
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-09-24 22:25:05 -0300

    Update 0_Home.py

--- pages/0_Home.py
+++ pages/0_Home.py
@@ -1,74 +1,79 @@
-import streamlit as st 
+import streamlit as st
 from PIL import Image
 import base64
 from io import BytesIO
 
 def main():
-    # Configuração da página
+    # Configura as preferências da página, como título, ícone e layout.
     st.set_page_config(page_title="DataThon", page_icon=":house:", layout='wide')
+    
+    # Exibe uma mensagem de sucesso no menu lateral.
     st.sidebar.success("Selecione uma página acima.")
 
-    # Carregar a imagem de fundo
+    # Carrega a imagem de fundo.
     image = Image.open("Imagens/capa4.png").convert("RGBA")
 
-    # Redimensionar a imagem
-    new_width = 1600  # Defina a nova largura desejada
-    new_height = 800  # Defina a nova altura desejada
+    # Redimensiona a imagem para se ajustar melhor à tela.
+    new_width = 1600  # Define a nova largura da imagem.
+    new_height = 800  # Define a nova altura da imagem.
     resized_image = image.resize((new_width, new_height))
 
-    # Converter a imagem redimensionada para base64
+    # Converte a imagem redimensionada para o formato base64 para ser usada no HTML.
     buffered = BytesIO()
     resized_image.save(buffered, format="PNG")
     img_str = base64.b64encode(buffered.getvalue()).decode()
 
-    # Adicionar a imagem de fundo e aplicar transparência
+    # Aplica a imagem de fundo e ajusta a transparência da interface.
     st.markdown(
         f"""
         <style>
         @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap');
         
+        /* Define o estilo de fundo da aplicação com a imagem carregada */
         .stApp {{
             background: url(data:image/png;base64,{img_str}) no-repeat center center fixed;
             background-size: cover;
-            color: #ffffff;  /* Cor do texto principal */
+            color: #ffffff;  /* Define a cor padrão do texto */
         }}
 
-        /* Centralizando o conteúdo */
+        /* Centraliza o conteúdo principal da página */
         .block-container {{
             padding: 5% 15%;
         }}
         
-        /* Estilo da caixa centralizada com leve transparência */
+        /* Estiliza a caixa de conteúdo central com leve transparência */
         .badge {{
-            background-color: rgba(255, 255, 255, 0.85);  /* Transparência da caixa */
+            background-color: rgba(255, 255, 255, 0.85);  /* Transparência da caixa de conteúdo */
             padding: 30px;
             border-radius: 15px;
-            box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);  /* Sombra */
+            box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);  /* Adiciona uma sombra suave */
             text-align: center;
             width: 80%;
             margin: auto;
         }}
 
+        /* Define o estilo do título principal */
         h1 {{
             font-family: 'Poppins', sans-serif;
             font-weight: 700;
-            color: #2e4053;  /* Título azul-acinzentado */
+            color: #2e4053;  /* Cor do título */
             text-align: center;
             font-size: 3em;  /* Tamanho do título */
             margin-bottom: 20px;
         }}
         
+        /* Define o estilo do subtítulo */
         h2 {{
             font-family: 'Poppins', sans-serif;
             font-weight: 400;
-            color: #1c2833;  /* Texto cinza escuro */
+            color: #1c2833;  /* Cor do texto do subtítulo */
             text-align: center;
             font-size: 1.2em;
             margin-bottom: 50px;
             line-height: 1.5;
         }}
 
-        /* Estilo para botões de navegação */
+        /* Estiliza os botões de navegação */
         .stButton>button {{
             background-color: #800020;
             color: #ffffff;
@@ -76,10 +81,11 @@ def main():
             padding: 10px 20px;
             border-radius: 5px;
             border: none;
-            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-            transition: background-color 0.3s ease;
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);  /* Adiciona uma leve sombra */
+            transition: background-color 0.3s ease;  /* Transição suave ao passar o mouse */
         }}
 
+        /* Muda a cor do botão ao passar o mouse */
         .stButton>button:hover {{
             background-color: #990033;
         }}
@@ -88,7 +94,7 @@ def main():
         unsafe_allow_html=True
     )
 
-    # Cabeçalho e descrição principal dentro da badge
+    # Adiciona o cabeçalho e a descrição principal em uma caixa centralizada e estilizada (badge).
     st.markdown(
         """
         <div class="badge">
