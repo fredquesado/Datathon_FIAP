8f73cf4773273fff5761f219626432fdc165d421
Vandrade85 <121468672+Vandrade85@users.noreply.github.com>
2024-09-24 21:57:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ Grupo = importlib.import_module('pages.1_Grupo')
 Desafio = importlib.import_module('pages.2_Desafio')
 Relatorio_Analitico_Preditivo = importlib.import_module('pages.3_Relatório_Analitico_Preditivo')
 Insights_e_Conclusoes = importlib.import_module('pages.5_Insights_e_Conclusoes')
-
-# Se o arquivo Home for necessário, importe também (caso o nome do arquivo tenha acento, use importlib)
 Home = importlib.import_module('pages.0_Home')
 
 # Mapeamento das páginas
@@ -19,5 +17,22 @@ PAGES = {
     "Insights e Conclusão": Insights_e_Conclusoes,
 }
 
-if __name__ == "__main__":
+# Definir a função main
+def main():
+    st.sidebar.title('Menu de Navegação')
+    
+    # Definir 'Home' como a página padrão
+    default_page = "Home"
+    
+    # Sidebar com valor padrão definido para "Home"
+    selection = st.sidebar.radio("Ir para", list(PAGES.keys()), index=list(PAGES.keys()).index(default_page))
+    
+    # Carregar a página selecionada
+    page = PAGES[selection]
+    
+    # Exibir o conteúdo da página selecionada
+    page.main()  # Supondo que cada módulo tenha uma função `main()` que renderiza a página
+
+# Chamar a função main dentro do bloco correto
+if __name__ == "__main__": 
     main()
